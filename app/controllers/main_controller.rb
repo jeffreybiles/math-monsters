7235b3a23a21cbd5d6eb068228992c5a11d4c625
Jeffrey Biles <jeffreybiles@Jeffreys-MacBook-Pro-2.local>
2014-04-01 19:42:02 -0500

    route to ember in rails

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def ember
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ MathMonsters::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'main#ember'
 end
